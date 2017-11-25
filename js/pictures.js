bd658acd28dc105d3ebc182aeb1ce4ddc92af42d
pashkes <pashkes@gmail.com>
2017-11-25 17:34:37 +0200

    Edited function render comments

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mockCommentsData = [
+var MOCK_COMMENTS_DATA = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -17,15 +17,15 @@ var getPhotoData = function () {
   var gallerySize = 26;
   var dataObjects = [];
   for (var i = 1; i < gallerySize; i++) {
-    // get random mockCommentsData
-    var firstCommentIndex = Math.round(getRangeRandomNumbers(0, Math.ceil(mockCommentsData.length - 1) / 2));
-    var secondCommentIndex = Math.round(getRangeRandomNumbers(Math.ceil((mockCommentsData.length - 1) / 2), mockCommentsData.length - 1));
+    // get random MOCK_COMMENTS_DATA
+    var firstCommentIndex = Math.round(getRangeRandomNumbers(0, Math.ceil(MOCK_COMMENTS_DATA.length - 1) / 2));
+    var secondCommentIndex = Math.round(getRangeRandomNumbers(Math.ceil((MOCK_COMMENTS_DATA.length - 1) / 2), MOCK_COMMENTS_DATA.length - 1));
     dataObjects[i - 1] = {
       url: 'photos/' + i + '.jpg',
       likes: getRangeRandomNumbers(15, 200),
       commentsData: [
-        mockCommentsData[firstCommentIndex],
-        mockCommentsData[secondCommentIndex]
+        MOCK_COMMENTS_DATA[firstCommentIndex],
+        MOCK_COMMENTS_DATA[secondCommentIndex]
       ]
     };
   }
@@ -40,18 +40,17 @@ var createDOMElement = function (dataPhoto) {
     itemElement.href = dataPhoto[i].url;
     itemElement.querySelector('img').src = dataPhoto[i].url;
     itemElement.querySelector('.picture-likes').textContent = dataPhoto.likes;
-    renderComments(dataPhoto, itemElement, i);
-    // commentsElement.remove();
+    renderComments(dataPhoto[i], itemElement);
     fragment.appendChild(itemElement);
   }
   return fragment;
 };
 
-var renderComments = function (comments, item, indexElement) {
-  for (var j = 0; j < comments[indexElement].commentsData.length; j++) {
+var renderComments = function (comments, item) {
+  for (var j = 0; j < comments.commentsData.length; j++) {
     var commentsElement = item.querySelectorAll('.picture-comments')[0];
     var cloneComments = commentsElement.cloneNode(true);
-    cloneComments.textContent = comments[indexElement].commentsData[j];
+    cloneComments.textContent = comments.commentsData[j];
     item.querySelector('.picture-stats').insertBefore(cloneComments, item.querySelector('.picture-likes'));
   }
   commentsElement.remove();
