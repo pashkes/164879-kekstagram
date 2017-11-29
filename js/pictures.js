19b51b5048154088cada6a17b152a6a11c5fcad9
pashkes <pashkes@gmail.com>
2017-11-29 12:58:54 +0200

    Added event click for pictures

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var HIDDEN_CLASS = 'hidden';
+var OVERLAY_CLASS = '.gallery-overlay';
+var OVERLAY_IMAGE_CLASS = '.gallery-overlay-image';
+var OVERLAY_COMMENTS_CLASS = '.comments-count';
+var OVERLAY_LIKES_COUNT = '.likes-count';
+
 var MOCK_COMMENTS_DATA = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -22,7 +28,7 @@ var getPhotoData = function () {
     var secondCommentIndex = Math.round(getRangeRandomNumbers(Math.ceil((MOCK_COMMENTS_DATA.length - 1) / 2), MOCK_COMMENTS_DATA.length - 1));
     dataObjects[i - 1] = {
       url: 'photos/' + i + '.jpg',
-      likes: getRangeRandomNumbers(15, 200),
+      likes: Math.floor(getRangeRandomNumbers(15, 200)),
       commentsData: [
         MOCK_COMMENTS_DATA[firstCommentIndex],
         MOCK_COMMENTS_DATA[secondCommentIndex]
@@ -39,7 +45,7 @@ var createDOMElement = function (dataPhoto) {
     var itemElement = templateItem.cloneNode(true);
     itemElement.href = dataPhoto[i].url;
     itemElement.querySelector('img').src = dataPhoto[i].url;
-    itemElement.querySelector('.picture-likes').textContent = dataPhoto.likes;
+    itemElement.querySelector('.picture-likes').textContent = dataPhoto[i].likes;
     renderComments(dataPhoto[i], itemElement);
     fragment.appendChild(itemElement);
   }
@@ -62,19 +68,47 @@ var renderPhoto = function (dataHTML) {
 };
 
 var renderPreview = function (dataArray) {
-  var overlay = document.querySelector('.gallery-overlay');
-  var mainImage = overlay.querySelector('.gallery-overlay-image');
-  var likes = overlay.querySelector('.likes-count');
-  var commentsCount = overlay.querySelector('.comments-count');
-  overlay.classList.remove('hidden');
+  var overlay = document.querySelector(OVERLAY_CLASS);
+  var mainImage = overlay.querySelector(OVERLAY_IMAGE_CLASS);
+  var likes = overlay.querySelector(OVERLAY_LIKES_COUNT);
+  var commentsCount = overlay.querySelector(OVERLAY_COMMENTS_CLASS);
   mainImage.src = dataArray[0].url;
-  likes.textContent = Math.round(getRangeRandomNumbers(15, 200));
-  commentsCount.textContent = dataArray[0].commentsData.length;
+  likes.textContent = Math.round(getRangeRandomNumbers(15, 200)).toString();
+  commentsCount.textContent = dataArray[0].commentsData.length.toString();
+};
+
+var overlay = document.querySelector(OVERLAY_CLASS);
+
+var showPicture = function () {
+  var pictures = document.querySelectorAll('.picture');
+  for (var i = 0; i < pictures.length; i++) {
+    pictures[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var currentTarget = evt.currentTarget;
+      var currentPicture = evt.target.src;
+      var mainPicture = overlay.querySelector(OVERLAY_IMAGE_CLASS);
+      var likesCount = currentTarget.querySelector('.picture-likes').textContent;
+      var commentCount = currentTarget.querySelectorAll('.picture-comments').length;
+      overlay.querySelector(OVERLAY_LIKES_COUNT).textContent = likesCount;
+      mainPicture.src = currentPicture;
+      overlay.querySelector(OVERLAY_COMMENTS_CLASS).textContent = commentCount.toString();
+      overlay.classList.remove(HIDDEN_CLASS);
+    });
+  }
+};
+
+var hiddenPicture = function () {
+  var closeButton = document.querySelector('.gallery-overlay-close');
+  closeButton.onclick = function () {
+    overlay.classList.add(HIDDEN_CLASS);
+  };
 };
 
 var renderGallery = function () {
   var photoGallery = getPhotoData();
   renderPhoto(photoGallery);
+  showPicture();
   renderPreview(photoGallery);
+  hiddenPicture();
 };
 renderGallery();
