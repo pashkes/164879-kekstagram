c37c783f70b77145886ebdde340b3ae751509361
pashkes <pashkes@gmail.com>
2017-11-30 11:54:42 +0200

    Added comments for all functions

--- js/pictures.js
+++ js/pictures.js
@@ -7,6 +7,7 @@ var OVERLAY_COMMENTS_CLASS = '.comments-count';
 var OVERLAY_LIKES_COUNT = '.likes-count';
 var ESC_KEY = 27;
 var ENTER_KEY = 13;
+var overlay = document.querySelector(OVERLAY_CLASS);
 var MOCK_COMMENTS_DATA = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -16,14 +17,21 @@ var MOCK_COMMENTS_DATA = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+/*
+ * Generating a random number in a fixed range of numbers.
+ */
 var getRangeRandomNumbers = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 };
 
+/*
+ * Generate an object based on artificial data.
+ * @returns {Array}
+ */
 var getPhotoData = function () {
-  var gallerySize = 26;
+  var GALLERY_SIZE = 26;
   var dataObjects = [];
-  for (var i = 0; i < gallerySize; i++) {
+  for (var i = 0; i < GALLERY_SIZE; i++) {
     // get random MOCK_COMMENTS_DATA
     var firstCommentIndex = getRangeRandomNumbers(0, Math.ceil(MOCK_COMMENTS_DATA.length - 1) / 2);
     var secondCommentIndex = getRangeRandomNumbers(Math.ceil((MOCK_COMMENTS_DATA.length - 1) / 2), MOCK_COMMENTS_DATA.length - 1);
@@ -39,6 +47,11 @@ var getPhotoData = function () {
   return dataObjects;
 };
 
+/*
+ * Creating a fragment of HTML elements.
+ * Retrieving data from an object.
+ * Write data to the current NODE item.
+ */
 var createDOMElement = function (dataPhoto) {
   var fragment = document.createDocumentFragment();
   var templateItem = document.getElementById('picture-template').content.querySelector('.picture');
@@ -53,6 +66,11 @@ var createDOMElement = function (dataPhoto) {
   return fragment;
 };
 
+/*
+ * Rendering a comment
+ * @param comments - массив данных
+ * @param item - NODE элемент в котором будет отрисовка комментариев
+ */
 var renderComments = function (comments, item) {
   for (var j = 0; j < comments.commentsData.length; j++) {
     var commentsElement = item.querySelectorAll('.picture-comments')[0];
@@ -63,32 +81,43 @@ var renderComments = function (comments, item) {
   commentsElement.remove();
 };
 
+/*
+ * Inserting an HTML snippet of items into a gallery container
+ * @param dataHTML - fragment of generated HTML elements
+ */
 var renderPhoto = function (dataHTML) {
   var pictures = document.querySelector('.pictures');
   pictures.appendChild(createDOMElement(dataHTML));
 };
 
-var renderPreview = function (dataArray) {
-  var overlay = document.querySelector(OVERLAY_CLASS);
-  var mainImage = overlay.querySelector(OVERLAY_IMAGE_CLASS);
+/*
+ * Drawing previews of pictures from data
+ */
+var renderPreviewPictures = function (dataArray) {
+  var bigPicture = overlay.querySelector(OVERLAY_IMAGE_CLASS);
   var likes = overlay.querySelector(OVERLAY_LIKES_COUNT);
   var commentsCount = overlay.querySelector(OVERLAY_COMMENTS_CLASS);
-  mainImage.src = dataArray[0].url;
+  bigPicture.src = dataArray[0].url;
   likes.textContent = Math.round(getRangeRandomNumbers(15, 200)).toString();
   commentsCount.textContent = dataArray[0].commentsData.length.toString();
 };
 
-var overlay = document.querySelector(OVERLAY_CLASS);
-
+/*
+ * Undo the default action for the link
+ * Interception of the ascent event on a link with a picture
+ * Retrieving data from the element by which the click occurred
+ * Set of received data in DOM overlay elements
+ */
 var insertCurrentContent = function (evt) {
   evt.preventDefault();
-  var currentTarget = evt.currentTarget;
-  var picturePatchFile = currentTarget.querySelector('img').src;
+  var target = evt.target;
+  var currentElement = target.closest('.picture');
+  var srcPicture = currentElement.querySelector('img').src;
   var mainPicture = overlay.querySelector(OVERLAY_IMAGE_CLASS);
-  var likesCount = currentTarget.querySelector('.picture-likes').textContent;
-  var commentCount = currentTarget.querySelectorAll('.picture-comments').length;
+  var likesCount = currentElement.querySelector('.picture-likes').textContent;
+  var commentCount = currentElement.querySelectorAll('.picture-comments').length;
   overlay.querySelector(OVERLAY_LIKES_COUNT).textContent = likesCount;
-  mainPicture.src = picturePatchFile;
+  mainPicture.src = srcPicture;
   overlay.querySelector(OVERLAY_COMMENTS_CLASS).textContent = commentCount.toString();
   overlay.classList.remove(HIDDEN_CLASS);
 };
@@ -97,36 +126,46 @@ var removeOverlayClass = function () {
   overlay.classList.add(HIDDEN_CLASS);
 };
 
+/*
+ * On the click event of pictures, insert data into the overlay received from the current item
+ */
 var showPicture = function () {
-  var pictures = document.querySelectorAll('.picture');
-  for (var i = 0; i < pictures.length; i++) {
-    pictures[i].addEventListener('click', insertCurrentContent);
-  }
+  var container = document.querySelector('.pictures');
+  container.addEventListener('click', insertCurrentContent);
 };
 
-var closeOverlay = function (evt) {
+/*
+ * Check which button is pressed.
+ * Closing an overlay with a picture.
+ */
+var checkKeyDown = function (evt) {
   if (evt.keyCode === ESC_KEY) {
     removeOverlayClass();
+    return;
   }
-  // else if (evt.keyCode === ENTER_KEY) {
-  //   removeOverlayClass();
-  // }
+  if (evt.keyCode === ENTER_KEY) {
+    removeOverlayClass();
+  }
 };
 
-var hiddenPicture = function () {
+/*
+ * If there is no class 'hidden' - add events (Click, keyDown) to the close button of the overlay and the overlay itself
+ */
+var closePicture = function () {
   var closeButton = document.querySelector('.gallery-overlay-close');
-  closeButton.addEventListener('click', removeOverlayClass);
-  closeButton.addEventListener('keyup', closeOverlay);
-  if (overlay.classList.contains('hidden')) {
-    document.addEventListener('keyup', closeOverlay);
+  if (!overlay.classList.contains('hidden')) {
+    return;
   }
+  closeButton.addEventListener('click', removeOverlayClass);
+  closeButton.addEventListener('keyDown', checkKeyDown);
+  document.addEventListener('keydown', checkKeyDown);
 };
 
 var renderGallery = function () {
   var photoGallery = getPhotoData();
   renderPhoto(photoGallery);
+  renderPreviewPictures(photoGallery);
   showPicture();
-  renderPreview(photoGallery);
-  hiddenPicture();
+  closePicture();
 };
 renderGallery();
