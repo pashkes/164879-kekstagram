7fbaba5cb6501acaef408831fbf320aa60d4678a
pashkes <pashkes@gmail.com>
2017-12-01 10:11:11 +0200

    added event for button close big overlay picture in Enter

--- js/pictures.js
+++ js/pictures.js
@@ -161,6 +161,7 @@ var checkKeyDown = function (event) {
 var addHandlerForClosePicture = function () {
   var closeButton = document.querySelector('.gallery-overlay-close');
   closeButton.addEventListener('click', removeOverlayClass);
+  closeButton.addEventListener('keydown', removeOverlayClass);
   document.addEventListener('keydown', checkKeyDown);
 };
 
