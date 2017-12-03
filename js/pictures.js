2c0fe844ae3d20dbf3aa76cbbe5943a39839b949
pashkes <pashkes@gmail.com>
2017-12-03 16:33:56 +0200

    Added show / hide settings for the downloaded image, added validation for form fields, added a superposition of filters and scale for the image

--- js/pictures.js
+++ js/pictures.js
@@ -169,3 +169,186 @@ var renderGallery = function () {
   addHandlerForClickOnPicture();
 };
 renderGallery();
+
+
+var uploadOverlay = document.querySelector('.upload-overlay');
+var closeButton = document.querySelector('.upload-form-cancel');
+
+var addHandlerForClosingUpload = function () {
+  closeButton.addEventListener('click', hideUploadOverlay);
+  closeButton.addEventListener('keydown', hideWhenKeyDownEnter);
+  document.addEventListener('keydown', hideWhenKeyDownEsc);
+};
+
+var removeClickCloseUpload = function () {
+  closeButton.removeEventListener('click', hideUploadOverlay);
+  document.removeEventListener('keydown', hideWhenKeyDownEsc);
+};
+
+var showUploadOverlay = function () {
+  uploadOverlay.classList.remove(HIDDEN_CLASS);
+  addHandlerForClosingUpload();
+  addFocusHandlerCommentsField();
+  addFilterSelector();
+};
+
+var hideUploadOverlay = function () {
+  uploadOverlay.classList.add(HIDDEN_CLASS);
+  removeClickCloseUpload();
+  resetFilterSelector();
+  generalResetImg();
+
+};
+
+var hideWhenKeyDownEsc = function (event) {
+  if (event.keyCode === ESC_KEY) {
+    hideUploadOverlay();
+    removeClickCloseUpload();
+    resetFilterSelector();
+    generalResetImg();
+  }
+};
+
+var hideWhenKeyDownEnter = function (event) {
+  if (event.keyCode === ESC_KEY) {
+    hideUploadOverlay();
+    removeClickCloseUpload();
+    resetFilterSelector();
+    generalResetImg();
+  }
+};
+
+var addFocusHandlerCommentsField = function () {
+  var commentsField = document.querySelector('.upload-form-description');
+  commentsField.addEventListener('focus', removeClickCloseUpload);
+  commentsField.addEventListener('blur', addHandlerForClosingUpload);
+};
+
+var addHandlerUploadPhoto = function () {
+  var areaUploadPicture = document.querySelector('.upload-input');
+  areaUploadPicture.addEventListener('change', showUploadOverlay);
+};
+addHandlerUploadPhoto();
+
+
+var resizeControls = document.querySelector('.upload-resize-controls');
+
+var handlerResizeButton = function (event) {
+  var zoomOut = event.target.closest('.upload-resize-controls-button-dec');
+  var zoomIn = event.target.closest('.upload-resize-controls-button-inc');
+  var zoomValue = resizeControls.querySelector('.upload-resize-controls-value');
+  var STEP_ZOOM = 25;
+  var MIN_ZOOM = STEP_ZOOM;
+  var MAX_ZOOM = 100;
+  var currentValueZoom = parseInt(zoomValue.value, 10);
+  if (zoomOut) {
+    if (currentValueZoom <= MIN_ZOOM || currentValueZoom > MAX_ZOOM) {
+      return;
+    }
+    currentValueZoom -= STEP_ZOOM;
+    zoomValue.value = currentValueZoom + '%';
+    setImgZoom(currentValueZoom, MAX_ZOOM);
+  }
+  if (zoomIn) {
+    if (currentValueZoom < MIN_ZOOM || currentValueZoom >= MAX_ZOOM) {
+      return;
+    }
+    currentValueZoom += STEP_ZOOM;
+    zoomValue.value = currentValueZoom + '%';
+    setImgZoom(currentValueZoom, MAX_ZOOM);
+  }
+};
+
+var zoomTheImage = function () {
+  resizeControls.addEventListener('click', handlerResizeButton);
+};
+zoomTheImage();
+
+var imgPreview = document.querySelector('.effect-image-preview');
+var filterSelection = function (event) {
+  var targetElement = event.target.closest('.upload-effect-label');
+  var currentFilterName = targetElement.htmlFor.replace('upload-', '');
+  if (!targetElement) {
+    return;
+  }
+  if (imgPreview.classList.length >= 2) {
+    imgPreview.classList.remove(imgPreview.classList[1]);
+  }
+  imgPreview.classList.add(currentFilterName);
+};
+
+var filtersContainer = document.querySelector('.upload-effect-controls');
+var addFilterSelector = function () {
+  filtersContainer.addEventListener('click', filterSelection);
+};
+
+var clearFilterImgOnClosing = function () {
+  imgPreview.classList.remove(imgPreview.classList[1]);
+};
+
+var resetFilterSelector = function () {
+  filtersContainer.removeEventListener('click', filterSelection);
+};
+
+var setImgZoom = function (value, maxValue) {
+  var levelScale = value / maxValue;
+  imgPreview.style.transform = 'scale(' + levelScale + ')';
+};
+
+var resetZoomImgOnClosing = function () {
+  imgPreview.style.transform = 'scale(1)';
+};
+
+var generalResetImg = function () {
+  clearFilterImgOnClosing();
+  resetZoomImgOnClosing();
+  resetValueField();
+};
+
+
+var hashTagsField = document.querySelector('.upload-form-hashtags');
+
+var checkValidHashTags = function (event) {
+  var hashTags = hashTagsField.value.toLowerCase().trim().split(' ').sort();
+  var maxAmount = 5;
+  var maxLength = 20;
+  var FIRST_INDEX = 0;
+  var minLength = 1;
+  var resetStyleError = true;
+  if (hashTags[0] === '') {
+    return;
+  }
+  for (var i = 0; i < hashTags.length; i++) {
+    if (hashTags.length > maxAmount
+      || hashTags[FIRST_INDEX] === ''
+      || hashTags[i][FIRST_INDEX] !== '#'
+      || hashTags[i] === ' '
+      || hashTags[i] === hashTags[i + 1]
+      || hashTags[i].length <= minLength
+      || hashTags[i].length >= maxLength) {
+      addStyleErrorForField();
+      event.preventDefault();
+      break;
+    }
+    addStyleErrorForField(resetStyleError);
+  }
+};
+
+var addStyleErrorForField = function (resetStyle) {
+  hashTagsField.style.boxShadow = '0 0 2px 2px red';
+  if (resetStyle) {
+    hashTagsField.style.boxShadow = 'none';
+  }
+};
+
+var resetValueField = function () {
+  var message = document.querySelector('.upload-form-description');
+  hashTagsField.value = '';
+  message.value = '';
+};
+
+hashTagsField.addEventListener('change', checkValidHashTags);
+
+var formSubmit = document.querySelector('.upload-form-submit');
+formSubmit.addEventListener('click', checkValidHashTags);
+formSubmit.addEventListener('submit', generalResetImg);
