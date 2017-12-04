d42a349faa947c9b08b527c8a47793d64a97d4db
pashkes <pashkes@gmail.com>
2017-12-05 00:39:11 +0200

    Refactoring. Added Russian comments

--- js/pictures.js
+++ js/pictures.js
@@ -127,7 +127,6 @@ var showBigPictureWithOverlay = function (event) {
 
 var removeOverlayClass = function () {
   overlay.classList.add(HIDDEN_CLASS);
-  removeHandlerClosePicture();
 };
 
 /*
@@ -138,18 +137,13 @@ var addHandlerForClickOnPicture = function () {
   container.addEventListener('click', showBigPictureWithOverlay);
 };
 
-var removeHandlerClosePicture = function () {
-  removeEventListener('click', addHandlerForClickOnPicture);
-  removeEventListener('keydown', addHandlerForClickOnPicture);
-};
-
 /*
  * Check which button is pressed.
  * Closing an overlay with a picture.
  */
 var checkKeyDown = function (event) {
   if ((event.keyCode === ESC_KEY) || (event.keyCode === ENTER_KEY)) {
-    removeOverlayClass();
+    totalResetOnClosing();
   }
 };
 
@@ -162,77 +156,38 @@ var addHandlerForClosePicture = function () {
   document.addEventListener('keydown', checkKeyDown);
 };
 
-var renderGallery = function () {
-  var photoGallery = getPhotoData();
-  renderPhoto(photoGallery);
-  renderPreviewPictures(photoGallery);
-  addHandlerForClickOnPicture();
-};
-renderGallery();
-
-
-var uploadOverlay = document.querySelector('.upload-overlay');
-var closeButton = document.querySelector('.upload-form-cancel');
-
-var addHandlerForClosingUpload = function () {
-  closeButton.addEventListener('click', hideUploadOverlay);
-  closeButton.addEventListener('keydown', hideWhenKeyDownEnter);
-  document.addEventListener('keydown', hideWhenKeyDownEsc);
-};
-
-var removeClickCloseUpload = function () {
-  closeButton.removeEventListener('click', hideUploadOverlay);
-  document.removeEventListener('keydown', hideWhenKeyDownEsc);
-};
-
-var showUploadOverlay = function () {
-  uploadOverlay.classList.remove(HIDDEN_CLASS);
-  addHandlerForClosingUpload();
-  addFocusHandlerCommentsField();
-  addFilterSelector();
-};
-
-var hideUploadOverlay = function () {
-  uploadOverlay.classList.add(HIDDEN_CLASS);
-  removeClickCloseUpload();
-  resetFilterSelector();
-  generalResetImg();
-
-};
-
-var hideWhenKeyDownEsc = function (event) {
-  if (event.keyCode === ESC_KEY) {
-    hideUploadOverlay();
-    removeClickCloseUpload();
-    resetFilterSelector();
-    generalResetImg();
-  }
-};
-
-var hideWhenKeyDownEnter = function (event) {
-  if (event.keyCode === ESC_KEY) {
-    hideUploadOverlay();
-    removeClickCloseUpload();
-    resetFilterSelector();
-    generalResetImg();
-  }
-};
-
-var addFocusHandlerCommentsField = function () {
-  var commentsField = document.querySelector('.upload-form-description');
-  commentsField.addEventListener('focus', removeClickCloseUpload);
-  commentsField.addEventListener('blur', addHandlerForClosingUpload);
-};
-
+/**
+ * Добавление обработчика на изменения поля загрузки фото
+ */
 var addHandlerUploadPhoto = function () {
   var areaUploadPicture = document.querySelector('.upload-input');
   areaUploadPicture.addEventListener('change', showUploadOverlay);
 };
-addHandlerUploadPhoto();
 
+/**
+ * Показать попап настройки загруженного изображения
+ * Добавляет хэндлеры которые нужны только внутри открытого состояния попапа
+ */
+var showUploadOverlay = function () {
+  uploadOverlay.classList.remove(HIDDEN_CLASS);
+  addHandlerForClosedState();
+  addFocusHandlerCommentsField();
+  addFilterSelector();
+  zoomTheImage();
+  addHandlerCheckValidHashTagsFocus();
+  resetWhenShipped();
+  checkForFormErrors();
+};
 
-var resizeControls = document.querySelector('.upload-resize-controls');
+var hideUploadOverlay = function () {
+  uploadOverlay.classList.add(HIDDEN_CLASS);
+};
 
+/*
+ * При клике на "+/-" увеличивать/уменьшать значени поля зума
+ * В зависимости от значения внутри поля значения зума вызывывать функции которая присваивает картинке
+ * текущее значение зума
+ */
 var handlerResizeButton = function (event) {
   var zoomOut = event.target.closest('.upload-resize-controls-button-dec');
   var zoomIn = event.target.closest('.upload-resize-controls-button-inc');
@@ -259,55 +214,170 @@ var handlerResizeButton = function (event) {
   }
 };
 
+/**
+ * Добавление обработчика клика для кнопок зума
+ */
 var zoomTheImage = function () {
   resizeControls.addEventListener('click', handlerResizeButton);
 };
-zoomTheImage();
 
+/**
+ * Добавление обработчика валидации хэш тегов при изменения значения
+ */
+var addHandlerCheckValidHashTagsFocus = function () {
+  hashTagsField.addEventListener('change', checkValidHashTags);
+};
+
+/**
+ * Добавление общего сброса всех полей и значения при отправке формы
+ */
+var resetWhenShipped = function () {
+  formSubmit.addEventListener('submit', totalResetOnClosing);
+};
+
+var checkForFormErrors = function () {
+  formSubmit.addEventListener('click', checkValidHashTags);
+};
+var renderGallery = function () {
+  var photoGallery = getPhotoData();
+  renderPhoto(photoGallery);
+  renderPreviewPictures(photoGallery);
+  addHandlerForClickOnPicture();
+  addHandlerUploadPhoto();
+};
+renderGallery();
+
+var uploadOverlay = document.querySelector('.upload-overlay');
+var closeButton = document.querySelector('.upload-form-cancel');
+var resizeControls = document.querySelector('.upload-resize-controls');
+var filtersContainer = document.querySelector('.upload-effect-controls');
+var hashTagsField = document.querySelector('.upload-form-hashtags');
+var formSubmit = document.querySelector('.upload-form-submit');
 var imgPreview = document.querySelector('.effect-image-preview');
+
+/*
+ * Добавление бработчиков которые нужны внутри попапа
+ */
+var addHandlerForClosedState = function () {
+  closeButton.addEventListener('click', hideUploadOverlay);
+  closeButton.addEventListener('keydown', hideWhenKeyDownEnter);
+  document.addEventListener('keydown', hideWhenKeyDownEsc);
+};
+
+/*
+ * Удаление обработчиков события для кнопки закрытия попапа и для документа
+ */
+var removeClickCloseUpload = function () {
+  closeButton.removeEventListener('click', hideUploadOverlay);
+  document.removeEventListener('keydown', hideWhenKeyDownEsc);
+};
+
+/*
+ * При нажатии ескейп сбрасывать все значения, удалять обработчики которые нужны внутри попапа
+ * скрывать попап
+ */
+var hideWhenKeyDownEsc = function (event) {
+  if (event.keyCode === ESC_KEY) {
+    totalResetOnClosing();
+  }
+};
+
+/*
+ * При нажатии ентер сбрасывать все значения, удалять обработчики которые нужны внутри попапа
+  * скрывать попап
+ */
+var hideWhenKeyDownEnter = function (event) {
+  if (event.keyCode === ENTER_KEY) {
+    totalResetOnClosing();
+  }
+};
+
+/**
+ * Если в фокусе - удалять обработчики закрытие попапа при нажатии эскейп
+ * Если вышел с фокуса добавлять этот обработчик обратно
+ */
+var addFocusHandlerCommentsField = function () {
+  var commentsField = document.querySelector('.upload-form-description');
+  commentsField.addEventListener('focus', removeClickCloseUpload);
+  commentsField.addEventListener('blur', addHandlerForClosedState);
+};
+
+/*
+ * Выбор фильтра
+ * Если нажатие вне фильтра - выйти из функции
+ * Получить for для текущего элемента, убрать префикс
+ * Если в изображения больше 2-х классов - удалить второй
+ * Иначе добавить класс выбранного фильтра для изображения
+ */
 var filterSelection = function (event) {
   var targetElement = event.target.closest('.upload-effect-label');
-  var currentFilterName = targetElement.htmlFor.replace('upload-', '');
   if (!targetElement) {
     return;
   }
+  var currentFilterName = targetElement.htmlFor.replace('upload-', '');
   if (imgPreview.classList.length >= 2) {
     imgPreview.classList.remove(imgPreview.classList[1]);
   }
   imgPreview.classList.add(currentFilterName);
 };
 
-var filtersContainer = document.querySelector('.upload-effect-controls');
+/**
+ * Добавить обработчик для выбора фильтров
+ */
 var addFilterSelector = function () {
   filtersContainer.addEventListener('click', filterSelection);
 };
 
-var clearFilterImgOnClosing = function () {
+/**
+ * Удаление текущего эффекта с изображения
+ */
+var removeFilter = function () {
   imgPreview.classList.remove(imgPreview.classList[1]);
 };
 
-var resetFilterSelector = function () {
+/**
+ * Удаление обработчика выбора фильтра
+ */
+var removeFilterSelector = function () {
   filtersContainer.removeEventListener('click', filterSelection);
 };
 
+/*
+ * Присвоение значение зума для картинки
+ */
 var setImgZoom = function (value, maxValue) {
   var levelScale = value / maxValue;
   imgPreview.style.transform = 'scale(' + levelScale + ')';
 };
 
+/**
+ * Сбросить зумм для картинки по умолчанию
+ */
 var resetZoomImgOnClosing = function () {
   imgPreview.style.transform = 'scale(1)';
 };
 
-var generalResetImg = function () {
-  clearFilterImgOnClosing();
+/**
+ * Сброс параметров для картинки загрузки
+ * Сброс фильтра, сброс уровня увеличения, сброс
+ * Удаление обработчиков событие для кнопки закрытия
+ * Удаление обработчика события для выбора фильтров
+ * Вызов функции закрытие попапа
+ */
+var totalResetOnClosing = function () {
+  removeFilter();
   resetZoomImgOnClosing();
   resetValueField();
+  removeClickCloseUpload();
+  removeFilterSelector();
+  hideUploadOverlay();
 };
 
-
-var hashTagsField = document.querySelector('.upload-form-hashtags');
-
+/*
+ * Валидация списка хэш тегов
+ * Если не валидно добавлять стиль ошибки полю
+ * Иначе сбрасывать стиль ошибки
+ */
 var checkValidHashTags = function (event) {
   var hashTags = hashTagsField.value.toLowerCase().trim().split(' ').sort();
   var maxAmount = 5;
@@ -334,6 +404,10 @@ var checkValidHashTags = function (event) {
   }
 };
 
+/*
+ * Добавление красной тени
+ * Если resetStyle передан в метод сбросывать красную тень
+ */
 var addStyleErrorForField = function (resetStyle) {
   hashTagsField.style.boxShadow = '0 0 2px 2px red';
   if (resetStyle) {
@@ -341,14 +415,11 @@ var addStyleErrorForField = function (resetStyle) {
   }
 };
 
+/**
+ * Сброс полей формы
+ */
 var resetValueField = function () {
   var message = document.querySelector('.upload-form-description');
   hashTagsField.value = '';
   message.value = '';
 };
-
-hashTagsField.addEventListener('change', checkValidHashTags);
-
-var formSubmit = document.querySelector('.upload-form-submit');
-formSubmit.addEventListener('click', checkValidHashTags);
-formSubmit.addEventListener('submit', generalResetImg);
