3a5494257e7b506240fac1c95cfe4d0b58fca7fb
pashkes <pashkes@gmail.com>
2017-12-16 19:36:27 +0200

    added error handler

--- js/backend.js
+++ js/backend.js
@@ -3,16 +3,25 @@
 (function () {
   var SERVER_URL = 'https://1510.dump.academy/kekstagram';
   var STATUS_OK = 200;
-  var setup = function (onSuccess) {
+  var TIME_WAIT = 10000;
+
+  var setup = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
+    xhr.timeout = TIME_WAIT;
     xhr.addEventListener('load', function () {
       if (xhr.status === STATUS_OK) {
         onSuccess(xhr.response);
+      } else {
+        onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
     return xhr;
   };
 
